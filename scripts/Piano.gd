800ae691c5ee05d5ff7cdd769713a39f5f0f7bc7
Snoutibur <157987813+snoutibur@users.noreply.github.com>
2025-03-14 18:26:16 -0600

    Name and numbered piano according to MIDI note

--- scripts/Piano.gd
+++ scripts/Piano.gd
@@ -2,20 +2,32 @@ extends Control
 
 @onready var octave_template = $Octave
 
-var num_octaves = 7
+var num_octaves = 3
+var curNote = 60
 
 func generate_keys(octaves: int):
-	print("Generating " + str(octaves) + " octaves")
+	print("Generating " + str(octaves) + " octaves using template: " + str(octave_template))
 	for i in range(octaves):
+		# Add the octaves
 		var new_octave = octave_template.duplicate()
 		new_octave.position.x = i * octave_template.get_rect().size.x
 		add_child(new_octave)
+
+		# ID the keys to a MIDI Note
+		for key in new_octave.get_children():
+			if key.name.begins_with("Key"):
+				key.set_meta("midi_note", curNote)  # Attach midi_note as metadata to the key
+				key.name = "Key" + str(curNote)
+				key.text = str(curNote) # Set the button's text to the MIDI note number
+				print("Assigned MIDI note ", str(curNote), " to ", key.name)
+				curNote+=1
+
 	# Remove the original template after duplication
 	octave_template.queue_free()
 
-func _ready():
-	print(octave_template)
+	print("Octave generation complete")
 
+func _ready():
 	if octave_template == null:
 		print("Octave template no exist!")
 	else:
