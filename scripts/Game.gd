f84fcbe0d90219caa80d762b2f10fca0565cd02d
Snoutibur <157987813+snoutibur@users.noreply.github.com>
2025-03-15 13:00:31 -0600

    Piano generation fixes
    Ordered in octaves

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -1,8 +1,5 @@
 extends Node2D
 
-const Utility = preload( "res://addons/midi/Utility.gd" )
-const SMF = preload( "res://addons/midi/SMF.gd" )
-
 # Audio player for the backing track, responsible for playing the song
 @onready var audio_player = $BackingSong
 # MIDI player, responsible for handling MIDI note events
@@ -19,47 +16,52 @@ func _ready():
 
 	spawnNote(60)
 
-# Calculates where the note should be
-func noteXpos(note:int) -> float:
-	var key_width = 20
-	var startX = 100
-	return startX + (note-21) * key_width
-
-# Spawns in notes
-func spawnNote(note:int) -> void:
-	var noteSprite = Sprite2D.new() # better to spawn instances of a note scene.
-	noteSprite.texture = preload("res://assets/uFurry.png")
-	noteSprite.position = Vector2(noteXpos(note), 0)
-	add_child(noteSprite)
-	# make notesprite fall
-
-
-func _on_midi_player_midi_event( channel, event ):
-	# channel is same as $GodotMidiPlayer.channel_status[track_id]
-	# event is event parameter. see SMF.gd and MidiPlayer.gd
-	# 	-> more information at "MIDIEvent" at https://bitbucket.org/arlez80/godot-midi-player/wiki/struct/SMF
+# MIDI EVENT!
+func _on_midi_player_midi_event(channel, event):
+	# Spawn notes
+	match event.type:
+		SMF.MIDIEventType.note_on:
+			print(event.note)
 
 	# Output event data to stdout
-	var event_string = ""
-	match event.type:
-		SMF.MIDIEventType.note_off:
-			event_string = "NoteOff %d" % event.note
-		SMF.MIDIEventType.note_on:
-			event_string = "NoteOn note[%d] velocity[%d]" % [ event.note, event.velocity ]
-		SMF.MIDIEventType.polyphonic_key_pressure:
-			event_string = "PolyphonicKeyPressure note[%d] value[%d]" % [ event.note, event.value ]
-		SMF.MIDIEventType.control_change:
-			event_string = "ControlChange number[%d] value[%d]" % [ event.number, event.value ]
-		SMF.MIDIEventType.program_change:
-			event_string = "ProgramChange #%d" % event.number
-		SMF.MIDIEventType.pitch_bend:
-			event_string = "PitchBend %d -> %f" % [ event.value, ( event.value / 8192.0 ) - 1.0 ]
-		SMF.MIDIEventType.channel_pressure:
-			event_string = "ChannelPressure %d" % event.value
-		SMF.MIDIEventType.system_event:
-			event_string = "SystemEvent %d" % event.args.type
+	if(Global.debug_print):
+		var event_string = ""
+		match event.type:
+			SMF.MIDIEventType.note_off:
+				event_string = "NoteOff %d" % event.note
+			SMF.MIDIEventType.note_on:
+				event_string = "NoteOn note[%d] velocity[%d]" % [ event.note, event.velocity ]
+			SMF.MIDIEventType.polyphonic_key_pressure:
+				event_string = "PolyphonicKeyPressure note[%d] value[%d]" % [ event.note, event.value ]
+			SMF.MIDIEventType.control_change:
+				event_string = "ControlChange number[%d] value[%d]" % [ event.number, event.value ]
+			SMF.MIDIEventType.program_change:
+				event_string = "ProgramChange #%d" % event.number
+			SMF.MIDIEventType.pitch_bend:
+				event_string = "PitchBend %d -> %f" % [ event.value, ( event.value / 8192.0 ) - 1.0 ]
+			SMF.MIDIEventType.channel_pressure:
+				event_string = "ChannelPressure %d" % event.value
+			SMF.MIDIEventType.system_event:
+				event_string = "SystemEvent %d" % event.args.type
 
-	print( channel, event, "channel:%d event-type:%s" % [
-	channel.number,
-	event_string,
-	])
+		print( channel, event, "channel:%d event-type:%s" % [
+		channel.number,
+		event_string,
+		])
+
+## SPAWNING NOTES ##
+func spawnNote(note:int) -> void:
+	# Load the note scene
+	var note_scene = preload("res://scenes/note.tscn")
+	var note_instance = note_scene.instantiate()
+
+	# Find the key
+	var target_key_path:String = "Piano/Oct" + str(Global.octaveOf(note)) + "/Key" + str(note)
+	var key: Node = get_node(target_key_path)
+
+	# Put the note where it's supposed to go
+	if key:
+		note_instance.position.x = 90
+		note_instance.position.y = 90
+	else:
+		push_error("Failed to get key.")
--- scripts/Piano.gd
+++ scripts/Piano.gd
@@ -1,15 +1,15 @@
 extends Control
 
+var curNote:int = 36
 @onready var octave_template = $Octave
-
-var num_octaves = 5
-var curNote = 60
 
 func generate_keys(octaves: int):
 	print("Generating " + str(octaves) + " octaves using template: " + str(octave_template))
+
 	for i in range(octaves):
 		# Add the octaves
 		var new_octave = octave_template.duplicate()
+		new_octave.name = "Oct" + str(Global.octaveOf(curNote))
 		new_octave.position.x = i * octave_template.get_rect().size.x
 		add_child(new_octave)
 
@@ -27,8 +27,22 @@ func generate_keys(octaves: int):
 
 	print("Octave generation complete")
 
+
+func highlightKey(note: int) -> void: #TODO: Select and highlight the right key
+	print("Highlighting note: " + str(note))
+
+	# Select note
+	var keyName:String = "Octave/$Key" + str(note)
+	var target: Node  = get_node(keyName)
+
+	if target:
+		target.modulate = Color(1,0,0)
+	else:
+		print("Note not found!")
+
+
 func _ready():
 	if octave_template == null:
 		print("Octave template no exist!")
 	else:
-		generate_keys(num_octaves)
+		generate_keys(5)
